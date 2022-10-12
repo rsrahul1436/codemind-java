243ef6de962941e06f946f3457df4b3053343231
rahul singh <96769284+rsrahul1436@users.noreply.github.com>
2022-10-12 23:20:54 +0530

    import java.util.*; public class file {     public static int isprime(int n)     {         int i;         if(n<=1)         {             return 0;         }         for(i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)             {                 return 0;             }         }         return 1;     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,c=0,k;         n=sc.nextInt();         int arr[]=new int[n];         int i;         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(isprime(arr[i])==1)             {                 if(arr[i]>=k)                 {                     c++;                 }             }         }         System.out.println(c);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class file
+{
+    public static int isprime(int n)
+    {
+        int i;
+        if(n<=1)
+        {
+            return 0;
+        }
+        for(i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0,k;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        int i;
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(isprime(arr[i])==1)
+            {
+                if(arr[i]>=k)
+                {
+                    c++;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
