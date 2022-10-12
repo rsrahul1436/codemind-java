b3847af27250e610133164033b243dd5fa4ad00d
rahul singh <96769284+rsrahul1436@users.noreply.github.com>
2022-10-12 20:28:09 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n-1;i++)         {             if(arr[i+1]>arr[i])             {                 c+=1;             }         }         if(c==n-1)         {             System.out.println("yes");         }         else         {             System.out.println("no");         }     } }

---
+++ Ascending_array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n-1;i++)
+        {
+            if(arr[i+1]>arr[i])
+            {
+                c+=1;
+            }
+        }
+        if(c==n-1)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
