1c8fee56138021606935e8f444ad1220afd57e34
rahul singh <96769284+rsrahul1436@users.noreply.github.com>
2022-10-13 15:44:32 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,i,c=0,s=0;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             s+=arr[i];         }         int se;         se=s/n;         for(i=0;i<n;i++)         {             if(se==arr[i])             {                 c=1;                 break;             }         }         if(c==1)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ average_element_is_in_array_or_not.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0,s=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s+=arr[i];
+        }
+        int se;
+        se=s/n;
+        for(i=0;i<n;i++)
+        {
+            if(se==arr[i])
+            {
+                c=1;
+                break;
+            }
+        }
+        if(c==1)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
