ae4180bed1679a989ee47bd86d3824b528e9ec27
rahul singh <96769284+rsrahul1436@users.noreply.github.com>
2022-10-13 08:59:37 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n,i,s=0;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2==0)             {                 int m=0;                 for(int j=0;j<i;j++)                 {                     if(arr[i]==arr[j])                     {                         m=1;                         break;                     }                 }                 if(m==0)                 {                     s+=1;                 }             }         }         System.out.println(s);     } }

---
+++ unique_even_count.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0)
+            {
+                int m=0;
+                for(int j=0;j<i;j++)
+                {
+                    if(arr[i]==arr[j])
+                    {
+                        m=1;
+                        break;
+                    }
+                }
+                if(m==0)
+                {
+                    s+=1;
+                }
+            }
+        }
+        System.out.println(s);
+    }
+}
